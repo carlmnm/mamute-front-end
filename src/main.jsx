fc48d313d715907f4fb3c11ff2315994f3189c03
carlmnm <carlossmiguell@gmail.com>
2023-06-12 02:26:17 -0300

    feat: sign up

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
-import './assets/styles/reset.css';
+/* import './assets/styles/reset.css'; */
+/* import '../public/styles.css'; */
+
 
 ReactDOM.render(
   <App />,
--- src/pages/InitialPage.jsx
+++ src/pages/InitialPage.jsx
@@ -1,10 +1,69 @@
+import { useState } from "react";
+import useSignUp from "../hooks/api/useSignUp";
+
 export default function InitialPage() {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const { loadingSignUp, signUp } = useSignUp();
+
+    async function submit(event) {
+        event.preventDefault()
+        try {
+            await signUp(name, email, password)
+            alert('cadastrado')
+        } catch (errors) {
+            alert(errors.response.data.message)
+        }
+        
+    }
+
     return (
-        <div class='flex w-screen h-screen bg-pattern justify-center items-center'>
-            <form>
-                <input placeholder='name' type="text"></input>
+        <div class="flex w-screen h-screen bg-pattern justify-center items-center">
+            <form class="bg-orange-800 shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={submit}>
+                <div class="mb-4">
+                    <label class="block text-white text-sm font-bold mb-2" for="username">
+                        Insira seu nome
+                    </label>
+                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="name"
+                        type="text"
+                        value={name}
+                        onChange={e => setName(e.target.value)}
+                        placeholder="nome" />
+                </div>
+                <div class="mb-4">
+                    <label class="block text-white text-sm font-bold mb-2" for="username">
+                        Agora escreva seu e-mail
+                    </label>
+                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="email"
+                        type="email"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                        placeholder="e-mail"
+                    />
+                </div>
+                <div class="mb-4">
+                    <label class="block text-white text-sm font-bold mb-2" for="username">
+                        Digite uma senha bem segura
+                    </label>
+                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="password"
+                        type="password"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                        placeholder="senha"
+                    />
+                </div>
+                <div class="flex items-center justify-between">
+                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" 
+                    type="submit">
+                        Sign In
+                    </button>
+                </div>
             </form>
-           {/* <h1 class="bg-red-600 w-[90px] h-[90px]">OMG, Hiiiiii!</h1>*/}
-        </div>
-    )
+        </div >
+    );
 }
